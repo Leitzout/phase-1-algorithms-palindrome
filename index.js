741ecd6ab387c2206cca7f907e3a04f1cf09e51c
Kaylee Leitzel <kayleeleitzel@gmail.com>
2022-01-15 01:06:42 -0700

    commit tree d7ca371530e8ffe0a41d85a64fc3b701d87fcabd

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  let w = word.length -1;
+  for( let i = 0 ; i < w/2 ;i++)
+  {
+    let x = word[i] ;//forward character
+    let y = word[w-i];//backward character
+    if( x != y)
+    {
+      return false;
+    }
+  }
+  return true;
 
-/* 
-  Add your pseudocode here
-*/
+}
 
 /*
   Add written explanation of your solution here
+- iterate over the string, forwards and backwards
+- See if letters match forwards and backwards - true
+- Does not matches - false
 */
 
 // You can run `node index.js` to view these console logs
